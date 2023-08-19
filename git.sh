c8db8ce2d4686941481e1492efb6b86836336a6a
sonictheprogrammer <92324146+sonictheprogrammer@users.noreply.github.com>
2023-08-19 13:40:16 -0400

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "This is a Git test. If you wanna do something, go ahead and do run a file."
+wait
+echo "If you want to quit the operations of this program, Press Ctrl+C (^C)"
+wait
+echo "Otherwise, Enjoy!"
+./run.sh
